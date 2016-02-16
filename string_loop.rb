3aae8c4fc3dfa3ed5acfd8f668694312d37211f4
Jesusbaby <olayemifolakemi@yahoo.com>
2016-02-16 14:21:12 +0100

    third-edited ruby excercise

--- string_loop.rb
+++ string_loop.rb
@@ -1,3 +1,3 @@
 array = ["i",'k','e','c','h','u','k','w','u','y','e','r','e','m','e','f','u','n','a',]
 array.each_with_index{|i, index|
-puts "#{index}  #{i.upcase}" }
+puts "#{index + 1}  #{i.upcase}" }
