9437ba03cac179e5b7e1816dd6f48b56d57b042b
Jesusbaby <olayemifolakemi@yahoo.com>
2016-02-16 15:51:34 +0100

    third-corrected while loop ruby excercise

--- string_loop.rb
+++ string_loop.rb
@@ -1,3 +1,8 @@
-array = ["i",'k','e','c','h','u','k','w','u','y','e','r','e','m','e','f','u','n','a',]
-array.each_with_index{|i, index|
-puts "#{index + 1}  #{i.upcase}" }
+name = ['i','k','e','c','h','u','k','w','u','y','e','r','e','m','e','f','u','n','a',]
+i = 0
+while (i < name.length)
+    puts "#{i} #{name[i].upcase}"
+	i += 1
+
+end
+
